34a2da9117c395cbca01d6bba23817d79031f008
Rawanna Mana Nawanna Anushka Chathuranga <60468885+Anushkach@users.noreply.github.com>
2024-02-26 14:57:39 +0530

    minor modification

--- status/progress.py
+++ status/progress.py
@@ -1,9 +1,9 @@
 import solara
 @solara.component
 def Page():
-    solara.ProgressLinear(value=True)
+    solara.ProgressLinear(value=True) # indeterminate
     solara.ProgressLinear(value=30,color="purple") # precentage progress
-    solara.ProgressLinear(value=False) # space remian in layout
+    solara.ProgressLinear(value=False) # Hidden, but space remian in layout
 
     solara.ProgressLinear(value=70,
                           color='green',
@@ -12,3 +12,10 @@ def Page():
                                  "background-color":"orange",
                                  "border-radius":"5px"},
                           classes=["class1","class2","class3"])
+    
+    solara.ProgressLinear(value=True,
+                          color='green',
+                          style={"width":"2000px",
+                                "height":"20px",
+                                "background-color":"black",
+                                "border-radius":"10px"})
